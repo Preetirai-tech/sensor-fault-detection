fb9796a862064051cbb52503570af4e72f3bc625
Preeti Rai <preetirai1812@yahoo.com>
2022-12-31 18:45:36 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+def get_requirements()->List[str]:
+  """
+  This function will return list of requirements
+  """
+  requirements_list:List[str] = []
+  return requirements_list
+
+setup(
+
+  name = "sensor",
+  version = "0.0.1",
+  author = "Preetirai",
+  author_email = "preetirai1812@yahoo.com",
+  packages = find_packages(),
+  install_requires = get_requirements(),
+)
