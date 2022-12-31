6c9af88cdf8af56822e4358f7c8c329a8154022b
Preeti Rai <preetirai1812@yahoo.com>
2022-12-31 19:22:31 +0530

    update

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -10,7 +10,7 @@ class MongoDBClient:
         try:
 
             if MongoDBClient.client is None:
-                mongo_db_url = "mongodb+srv://avnish:XglZZ9OkjjUw74pZ@ineuron-ai-projects.7eh1w4s.mongodb.net/admin?authSource=admin&replicaSet=atlas-okvkrd-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"
+                mongo_db_url = "mongodb+srv://Preeti:preeti123@cluster0.qppgh02.mongodb.net/?retryWrites=true&w=majority"
                 MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
       
             self.client = MongoDBClient.client
